f3c4492d9a5e9cc08caa4460cfb975c21b9a5017
Evie Hoang <evie.h0325@gmail.com>
2023-11-25 10:44:13 -0800

    neaten up code

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
 const db = require('./db/db.json')
+
+const PORT = process.env.PORT || 3001
 
 //Allows all notes to have a unique ID
 const { v4: uuidv4 } = require('uuid');
